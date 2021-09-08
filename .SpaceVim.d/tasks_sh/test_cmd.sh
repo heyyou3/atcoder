143fd86ea299129dee8868b9b23a3754dca762a2
heyyou <hey3you3@gmail.com>
2021-09-08 22:51:36 +0900

    remove: .SpaceVim.d を削除

--- .SpaceVim.d/tasks_sh/test_cmd.sh
+++
@@ -1,4 +0,0 @@
-prob=$(perl -sE '@s=split(/\//, $fd_name);say $s[0]' -- -fd_name=$1)
-prop_p=$(perl -sE '@s=split(/\//, $fd_name);say $s[1]' -- -fd_name=$1)
-container_hash=$(docker ps | perl -anle 'if(/atcoder_dev_env_image/){print($F[0])}')
-docker exec $container_hash /bin/bash -c "cd /home/atcoder/$prob; make test q=$prop_p"
