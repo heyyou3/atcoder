ea234568f61549e7d1cba0ee0e12a631f127f7d2
heyyou <hey3you3@gmail.com>
2021-09-12 22:53:20 +0900

    update: Add test settings

--- templates/go/main.go
+++ templates/go/main.go
@@ -1,13 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+)
+
+func nextLine(scanner *bufio.Scanner) string {
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func nextInts(scanner *bufio.Scanner) (res []int) {
+	scanner.Scan()
+	s := scanner.Text()
+	for _, v := range strings.Split(s, " ") {
+		i, _ := strconv.Atoi(v)
+		res = append(res, i)
+	}
+	return
+}
 
 func resolver() int {
 	return 0
 }
 
 func main() {
-	var a int
-	fmt.Scan(&a)
-	fmt.Println(a)
 }
--- templates/go/main_test.go
+++ templates/go/main_test.go
@@ -1,16 +1,35 @@
 package main
 
-import "testing"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
-func Test_resolver(t *testing.T) {
-	tests := []struct {
-		name string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			resolver()
-		})
-	}
+var (
+	PROB = "a"
+)
+
+type sampleTestCase struct {
+	out string
+}
+
+func (stc *sampleTestCase) OutputFileName(sampleFileName string) string {
+	wd, _ := os.Getwd()
+	return fmt.Sprintf("%s/tests/%s.out", wd, sampleFileName)
+}
+
+func (stc *sampleTestCase) IntOutput() int {
+	outI, _ := strconv.Atoi(stc.out)
+	return outI
+}
+
+func NewSampleCase(sampleFileName string) *sampleTestCase {
+	stc := &sampleTestCase{}
+	outFile, _ := os.Open(stc.OutputFileName(sampleFileName))
+	outSc := bufio.NewScanner(outFile)
+	outSc.Scan()
+	stc.out = outSc.Text()
+	return stc
 }
